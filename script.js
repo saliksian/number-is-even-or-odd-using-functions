b5c08fcb0d5235f40d2bb77e807784aa3fdbe7c7
Muhammad Salik <75317422+saliksian@users.noreply.github.com>
2021-04-21 14:43:24 +0500

    final commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function check(num1) {
+    if (num1%2==0) {
+        console.log("Number is even!");
+    }
+    else if (num1 == 0) {
+        console.log("Number is zero!");
+    }
+    else{
+        console.log("Number is odd!!");
+    }
+    
+}
+
+let number = 22;
+check(number);
